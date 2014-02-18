9d1a8e65a1bcec2960ff9aa76ccabdc26a03a859
mrabinovitch <mrabinovitch@dalet.com>
2014-02-18 16:03:00 +0200

    cleanup before restart

--- src/com/dalet/svnstats/Main.java
+++ src/com/dalet/svnstats/Main.java
@@ -2,6 +2,9 @@ package com.dalet.svnstats;
 
 import org.tmatesoft.svn.core.io.SVNRepository;
 
+import java.io.File;
+import java.io.IOException;
+
 /**
  * User: Moisei Rabinovich
  * Date: 2/12/14
@@ -9,12 +12,18 @@ import org.tmatesoft.svn.core.io.SVNRepository;
  */
 public class Main {
     public static void main(String[] args) throws Exception {
+        String dbpath = "C:\\Sources\\DaletAddons\\Svn-Statistics\\.svnlogDB";
+        File dbfile = new File(dbpath);
+        Runtime.getRuntime().exec(new String[]{"cmd", "/k", "rmdir", "/s", "/q", dbfile.getCanonicalPath()});
+        if (dbfile.exists()) {
+            throw new IOException("Can't delete " + dbpath);
+        }
         SvnlogDbIndexer logDbBuilder = new SvnlogDbIndexer("svn://gfn-svn:3692");
-//        logDbBuilder.svnlog2db(82994, SVNRepository.INVALID_REVISION);
-        logDbBuilder.svnlog2db(152000, SVNRepository.INVALID_REVISION);
+        logDbBuilder.svnlog2db(82994, SVNRepository.INVALID_REVISION);
+//        logDbBuilder.svnlog2db(152000, SVNRepository.INVALID_REVISION);
 //        logDbBuilder.svnlog2db(126991, SVNRepository.INVALID_REVISION);
 //        logDbBuilder.svnlog2db(82994, 126990);
         logDbBuilder.close();
+        Runtime.getRuntime().exec(new String[]{"cmd", "/k", "C:\\Sources\\DaletAddons\\Svn-Statistics\\svnlogdb.bat"});
     }
-
 }
