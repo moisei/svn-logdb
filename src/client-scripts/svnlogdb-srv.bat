1e8754807c9518c8e18bea69769ccef680553dd1
Moisei <moisei@noreply.com>
2017-02-20 12:55:51 +0200

    allow custom init for java in batches.

--- src/client-scripts/svnlogdb-srv.bat
+++ src/client-scripts/svnlogdb-srv.bat
@@ -1,8 +1,8 @@
 @echo on
 @setlocal
-REM @call setjava > nul 2>&1
-set JAVA_HOME=C:\Program Files (x86)\Java\jre7-64
-set path=%JAVA_HOME%\bin;%path%
+@call %~dp0setjava.bat > nul 2>&1
+@rem set JAVA_HOME=C:\Program Files (x86)\Java\jre7-64
+@rem set path=%JAVA_HOME%\bin;%path%
 set cp=%~dp0hsqldb.jar;C:\Sources\vendor\hsqldb\hsqldb-2.3.2\lib\hsqldb.jar
 call java -version
 call java -cp -Xmx4024M -cp "%cp%" org.hsqldb.Server -database.0 "%~dp0.svnlogDB\db" -dbname.0 svnlogdb
--- src/client-scripts/svnlogdb.bat
+++ src/client-scripts/svnlogdb.bat
@@ -1,6 +1,6 @@
 @echo on
 @setlocal
-@call setjava > nul 2>&1
+@call %~dp0setjava.bat > nul 2>&1
 set cp=%~dp0hsqldb.jar;C:\Sources\vendor\hsqldb\hsqldb-2.3.2\lib\hsqldb.jar
 start javaw -Xmx1024M -cp "%cp%" org.hsqldb.util.DatabaseManagerSwing -url "jdbc:hsqldb:%~dp0.svnlogDB\db" --user sa --script "%~dp0svnlogdb-sample-scripts.sql"
 @endlocal
