7cb7805d4752dd8b8d6c2ec98a946f6818f76e09
Moisei <moisei@noreply.com>
2017-03-09 11:23:09 +0200

    add user groups script. to be updated manually.

--- src/client-scripts/svnlogdb-srv.bat
+++ src/client-scripts/svnlogdb-srv.bat
@@ -1,9 +1,6 @@
 @echo on
 @setlocal
 @call %~dp0setjava.bat > nul 2>&1
-@rem set JAVA_HOME=C:\Program Files (x86)\Java\jre7-64
-@rem set path=%JAVA_HOME%\bin;%path%
-set cp=%~dp0hsqldb.jar;C:\Sources\vendor\hsqldb\hsqldb-2.3.2\lib\hsqldb.jar
-call java -version
+set cp=%~dp0..\lib\hsqldb-2.3.4.jar
 call java -cp -Xmx4024M -cp "%cp%" org.hsqldb.Server -database.0 "%~dp0.svnlogDB\db" -dbname.0 svnlogdb
 @endlocal
